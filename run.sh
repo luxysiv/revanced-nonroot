4ebe6788428ae4288d1582cb9add2f1b91ae91b8
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-01-05 00:19:51 +0700

    Explain code

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+
+# Source YouTube version from file
 source ytversion.txt
 
+# Declare repositories
 declare -A repositories=(
     ["revanced-cli"]="revanced/revanced-cli"
     ["revanced-patches"]="revanced/revanced-patches"
     ["revanced-integrations"]="revanced/revanced-integrations"
 )
 
+# Download latest releases for specified repositories
 for repo in "${!repositories[@]}"; do
     response=$(wget -nv -O- "https://api.github.com/repos/${repositories[$repo]}/releases/latest")
 
@@ -20,10 +24,13 @@ for repo in "${!repositories[@]}"; do
     done
 done
 
+# Download YouTube APK
 wget -nv -O "youtube-v$version.apk" "$(echo $YT_URL | sed 's/&dl=0/&dl=1/')"
 
+# Read patches from file
 mapfile -t lines < ./patches.txt
 
+# Process patches
 for line in "${lines[@]}"; do
     if [[ -n "$line" && ( ${line:0:1} == "+" || ${line:0:1} == "-" ) ]]; then
         patch_name=$(sed -e 's/^[+|-] *//;s/ *$//' <<< "$line") 
@@ -32,18 +39,20 @@ for line in "${lines[@]}"; do
     fi
 done
 
+# Apply patches using Revanced tools
 java -jar revanced-cli*.jar patch \
     --merge revanced-integrations*.apk \
     --patch-bundle revanced-patches*.jar \
     "${exclude_patches[@]}" \
     "${include_patches[@]}" \
-    --out patched-youtube-v$version.apk \
-    youtube-v$version.apk
+    --out "patched-youtube-v$version.apk" \
+    "youtube-v$version.apk"
 
+# Sign the patched APK
 apksigner=$(find "$ANDROID_SDK_ROOT/build-tools" -name apksigner | sort -r | head -n 1)
 "$apksigner" sign --ks public.jks \
     --ks-key-alias public \
     --ks-pass pass:public \
     --key-pass pass:public \
-    --in patched-youtube-v$version.apk \
+    --in "patched-youtube-v$version.apk" \
     --out "youtube-revanced-v$version.apk"
