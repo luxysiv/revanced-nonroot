962a617556b44799d775ddab635e6eba1622f376
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-03-14 08:42:52 +0700

    Fix uptodown get final download link

--- revanced.sh
+++ revanced.sh
@@ -85,7 +85,9 @@ uptodown() {
     )
     url="https://dw.uptodown.com/dwn/$( \
         req - "$url" | \
-        pup -p --charset utf-8 'div[class="post-download"] attr{data-url}' \
+        pup -p --charset utf-8 'div.post-download[data-url]' | \
+        pup -p --charset utf-8 'div.post-download[data-url]:first-child' attr{data-url} \
+    
     )"
     req $name-v$version.apk "$url"
 }
