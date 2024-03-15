01ec9d34c8fe7610e1b05a0687431ad919251500
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-03-15 21:44:00 +0700

    Use APKmirror

--- revanced.sh
+++ revanced.sh
@@ -224,13 +224,19 @@ check_release_body() {
 
 # Activity patches APK
 patch() {
-    uptodown "youtube" \
-             "com.google.android.youtube"
+    apkmirror "google-inc" \
+              "youtube" \
+              "universal" \
+              "nodpi" \
+              "com.google.android.youtube"
     apply_patches "youtube"
     sign_patched_apk "youtube"
     create_github_release "youtube"
-    uptodown "youtube-music" \
-             "com.google.android.apps.youtube.music"
+    apkmirror "google-inc" \
+              "youtube-music" \
+              "arm64-v8a" \
+              "nodpi" \
+              "com.google.android.apps.youtube.music"
     apply_patches "youtube-music"
     sign_patched_apk "youtube-music"
     create_github_release "youtube-music"
