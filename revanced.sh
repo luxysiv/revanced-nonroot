5efc3aed45f8043fbf47c2d835e43fd9d6ff7d86
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-03-16 05:19:14 +0700

    Uptodown source

--- revanced.sh
+++ revanced.sh
@@ -224,19 +224,13 @@ check_release_body() {
 
 # Activity patches APK
 patch() {
-    apkmirror "google-inc" \
-              "youtube" \
-              "universal" \
-              "nodpi" \
-              "com.google.android.youtube"
+    uptodown "youtube" \
+             "com.google.android.youtube"
     apply_patches "youtube"
     sign_patched_apk "youtube"
     create_github_release "youtube"
-    apkmirror "google-inc" \
-              "youtube-music" \
-              "arm64-v8a" \
-              "nodpi" \
-              "com.google.android.apps.youtube.music"
+    uptodown "youtube-music" \
+             "com.google.android.apps.youtube.music"
     apply_patches "youtube-music"
     sign_patched_apk "youtube-music"
     create_github_release "youtube-music"
