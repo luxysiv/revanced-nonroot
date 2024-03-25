bf75a7e2e70c2d0326d0dde75b40eaad8cb8ba77
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-03-25 08:09:37 +0700

    Test Apkpure

--- revanced.sh
+++ revanced.sh
@@ -170,16 +170,13 @@ check_release_body() {
 
 # Activity patches APK
 patch() {
-    apkmirror "google-inc" \
-              "youtube" \
-              "com.google.android.youtube"
+    apkpure "youtube" \
+            "com.google.android.youtube"
     apply_patches "youtube"
     sign_patched_apk "youtube"
     create_github_release "youtube"
-    apkmirror "google-inc" \
-              "youtube-music" \
-              "com.google.android.apps.youtube.music" \
-              "arm64-v8a"
+    apkpure "youtube-music" \
+            "com.google.android.apps.youtube.music"
     apply_patches "youtube-music"
     sign_patched_apk "youtube-music"
     create_github_release "youtube-music"
