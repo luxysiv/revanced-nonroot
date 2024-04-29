5197767072da40a881f01c6a0ec804248727edfc
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-04-29 11:29:43 +0700

    Take APKs from APKmirror

--- revanced.sh
+++ revanced.sh
@@ -2,22 +2,23 @@
 source ./etc/utils.sh
 
 # Main script 
-# I prefer APKmirror but some time It's failure. So I use Uptodown.
-# APKmirror scraper still works fine
 
 # Perform download_repository_assets
 download_resources
 
 # Patch YouTube 
-uptodown "youtube" \
-         "com.google.android.youtube"
+apkmirror "google-inc" \
+          "youtube" \
+          "com.google.android.youtube"
 apply_patches "youtube"
 sign_patched_apk "youtube"
 create_github_release "youtube"
 
 # Patch YouTube Music 
-uptodown "youtube-music" \
-         "com.google.android.apps.youtube.music" 
+apkmirror "google-inc" \
+          "youtube-music" \
+          "com.google.android.apps.youtube.music" \
+          "arm64-v8a"
 apply_patches "youtube-music"
 sign_patched_apk "youtube-music"
 create_github_release "youtube-music"
