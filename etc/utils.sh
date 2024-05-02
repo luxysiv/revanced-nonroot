a3cd3e4ea545830a5f57b6f41b8a3e11935afb8b
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-05-02 21:31:14 +0700

    Make requests like send from Firefox Android

--- etc/utils.sh
+++ etc/utils.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Script make by Mạnh Dương
 
-# Make requests with some information 
+# Make requests like send from Firefox Android 
 req() {
-    wget --keep-session-cookies -nv -O "$@" --timeout=10 \
-         --header="User-Agent: Mozilla/5.0 (Linux; Android 10; K) \
-                          AppleWebKit/537.36 (KHTML, like Gecko) \
-                          Chrome/126.0.0.0 Mobile Safari/537.36 EdgA/126.0.0.0" \
-         --header="Connection: keep-alive" \
-         --header="Accept-Language: en-US,en;q=0.9" \
+    wget --header="User-Agent: Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0" \
          --header="Content-Type: application/octet-stream" \
-         --header="Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+         --header="Accept-Language: en-US,en;q=0.9" \
+         --header="Connection: keep-alive" \
+         --header="Upgrade-Insecure-Requests: 1" \
+         --header="Cache-Control: max-age=0" \
+         --header="Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" \
+         --keep-session-cookies --timeout=10 -nv -O "$@"
 }
 
 # Get highest version (Just compatible with my way of getting versions code)
