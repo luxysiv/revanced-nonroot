bbee90a276fa6be59eb3d48f57b99db8e19ef8dd
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-05-16 22:48:34 +0700

    Perl apply patches

---
+++ utils/apply_patches.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use File::Glob ':glob';
+
+# Get the arguments
+my ($name, $version) = @ARGV;
+
+# Process patches and store in an array
+my @optionsPatches = `perl utils/process_patches.pl $name`;
+chomp @optionsPatches;
+
+# Find the necessary files using wildcard
+my ($cli_jar) = bsd_glob("revanced-cli*.jar");
+my ($integrations_apk) = bsd_glob("revanced-integrations*.apk");
+my ($patches_jar) = bsd_glob("revanced-patches*.jar");
+
+# Construct the command
+my $cmd = "java -jar $cli_jar patch "
+        . "-m $integrations_apk "
+        . "-b $patches_jar "
+        . "@optionsPatches "
+        . "-o patched-$name-v$version.apk "
+        . "$name-v$version.apk";
+
+# Execute the command
+system($cmd) == 0 or die "Failed to execute: $cmd";
+
+# Remove the original APK
+unlink "$name-v$version.apk" or warn "Could not unlink $name-v$version.apk: $!";
+
+# Unset the optionsPatches array
+undef @optionsPatches;
+
+# Sign patched APK
+system("perl utils/sign_apk.pl $name $version") == 0 or die "Failed to execute sign_apk.pl";
