e1fc5714afbd4a3edc9e1a46eab82bc1f74315ad
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-05-17 11:27:05 +0700

    Change to download Github Assets

---
+++ utils/github_downloader.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use JSON;
+use File::Temp qw(tempfile);
+
+sub req {
+    my ($url, $output) = @_;
+    
+    my $command = "wget -nv -O \"$output\" \"$url\"";
+    system($command) == 0
+        or die "Failed to execute $command: $?";
+}
+
+sub download_resources {
+    my @repos = qw(revanced-patches revanced-cli revanced-integrations);
+
+    foreach my $repo (@repos) {
+        my $github_api_url = "https://api.github.com/repos/revanced/$repo/releases/latest";
+        my ($fh, $tempfile) = tempfile();
+
+        req($github_api_url, $tempfile);
+
+        open my $json_fh, '<', $tempfile or die "Could not open temporary file: $!";
+        my $content = do { local $/; <$json_fh> };
+        close $json_fh;
+
+        my $release_data = decode_json($content);
+        for my $asset (@{$release_data->{assets}}) {
+            my $asset_name = $asset->{name};
+            
+            # Skip files with .asc extension
+            next if $asset_name =~ /\.asc$/;
+
+            my $download_url = $asset->{browser_download_url};
+            req($download_url, $asset_name);
+        }
+
+        unlink $tempfile; # Remove the temporary JSON file
+    }
+}
+
+download_resources();
