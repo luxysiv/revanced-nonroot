b39b1760de5f0ae4dee88a4b08e1f182d40eb9ac
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-05-20 20:59:02 +0700

    Main script

---
+++ revanced.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use apkpure qw(apkpure);
+use uptodown qw(uptodown);
+use apkmirror qw(apkmirror);
+use apply_patches qw(apply_patches);
+use github_release qw(github_release);
+use github_downloader qw(download_resources);
+
+
+# Main
+
+# Download Github releases assets 
+download_resources();
+
+# Patch YouTube
+apkmirror(
+    "google-inc", 
+    "youtube", 
+    "com.google.android.youtube"
+);
+apply_patches("youtube");
+github_release("youtube");
+
+# Patch YouTube Music 
+apkmirror(
+    "google-inc", 
+    "youtube-music", 
+    "com.google.android.apps.youtube.music", 
+    "arm64-v8a"
+);
+apply_patches("youtube-music");
+github_release("youtube-music");
+
+# You can add other apps here
