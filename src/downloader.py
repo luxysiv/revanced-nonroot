6aaae1f52a2a83122dbc73dd3277fbbb06fd1e56
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-06-02 08:32:20 +0700

    Improve code
    Shorter code
    Add detect_github_link function 
    Add get_supported_version

--- src/downloader.py
+++ src/downloader.py
@@ -1,5 +1,4 @@
 import json
-import logging
 
 from src import apkmirror, apkpure, uptodown, version, scraper 
 
@@ -17,14 +16,9 @@ def download_resource(url: str, name: str) -> str:
                 file.write(chunk)
                 downloaded_size += len(chunk)
 
-        logging.info(
-            f"URL: {url} [{downloaded_size}/{total_size}] -> {name}"
-        )
-
     return filepath
 
 def download_required(source: str) -> dict:
-    logging.info("Downloading required resources")
     downloaded_files = {}
     base_url = "https://api.github.com/repos/{}/{}/releases/{}"
 
@@ -40,71 +34,77 @@ def download_required(source: str) -> dict:
         repo = repo_info.get('repo', "")
         tag = repo_info.get('tag', "")
 
-        if tag in ["", "dev", "prerelease"]:
-            url = f"https://api.github.com/repos/{user}/{repo}/releases"
-            response = scraper.get(url)
-            releases = response.json()
-
-            if tag == "":
-                latest_release = max(releases, key=lambda x: x['created_at'])
-            elif tag == "dev":
-                dev_releases = [release for release in releases if 'dev' in release['tag_name']]
-                latest_release = max(dev_releases, key=lambda x: x['created_at'])
-            else:  
-                pre_releases = [release for release in releases if release['prerelease']]
-                latest_release = max(pre_releases, key=lambda x: x['created_at'])
-
-            latest_tag_name = latest_release['tag_name']
-            url = base_url.format(user, repo, f"tags/{latest_tag_name}")
-        else:
-            url = base_url.format(user, repo, tag)
-
+        url = detect_github_link(base_url, user, repo, tag)
         response = scraper.get(url)
         assets = response.json().get("assets", [])
 
         for asset in assets:
-            if asset["name"].endswith(".asc"):
-                continue 
-            filepath = download_resource(asset["browser_download_url"], asset["name"])
-            downloaded_files[repo_info['repo'].replace("/", "")] = filepath
+            if not asset["name"].endswith(".asc"):
+                filepath = download_resource(asset["browser_download_url"], asset["name"])
+                downloaded_files[repo_info['repo'].replace("/", "")] = filepath
 
     return downloaded_files
-    
+
+
+def detect_github_link(base_url: str, user: str, repo: str, tag: str) -> str:
+    if tag in ["", "dev", "prerelease"]:
+        url = f"https://api.github.com/repos/{user}/{repo}/releases"
+        response = scraper.get(url)
+        releases = response.json()
+
+        if tag == "":
+            latest_release = max(releases, key=lambda x: x['created_at'])
+        elif tag == "dev":
+            dev_releases = [release for release in releases if 'dev' in release['tag_name']]
+            latest_release = max(dev_releases, key=lambda x: x['created_at'])
+        else:
+            pre_releases = [release for release in releases if release['prerelease']]
+            latest_release = max(pre_releases, key=lambda x: x['created_at'])
+
+        latest_tag_name = latest_release['tag_name']
+        return base_url.format(user, repo, f"tags/{latest_tag_name}")
+    else:
+        return base_url.format(user, repo, tag)
+
+
+def get_supported_version(package_name):
+    with open("./patches.json", "r") as patches_file:
+        patches = json.load(patches_file)
+    versions = set()
+    for patch in patches:
+        compatible_packages = patch.get("compatiblePackages")
+        if compatible_packages and isinstance(compatible_packages, list):
+            for package in compatible_packages:
+                if (
+                    package.get("name") == package_name and
+                    package.get("versions") is not None and
+                    isinstance(package["versions"], list) and
+                    package["versions"]
+                ):
+                    versions.update(
+                        map(
+                            str.strip, package["versions"]
+                        )
+                    )
+    if versions:
+        return sorted(versions, reverse=True)[0]
+    return None
+
+
 def download_apkmirror(app_name: str) -> str:
     global version
 
     try:
-        with open("./patches.json", "r") as patches_file:
-            patches = json.load(patches_file)
-
         conf_file_path = f'./apps/apkmirror/{app_name}.json'
 
         with open(conf_file_path, 'r') as json_file:
             config = json.load(json_file)
 
         version = config['version']
-        
-        if not version:
-            versions = set()
-            for patch in patches:
-                compatible_packages = patch.get("compatiblePackages")
-                if compatible_packages and isinstance(compatible_packages, list):
-                    for package in compatible_packages:
-                        if (
-                            package.get("name") == config['package'] and
-                            package.get("versions") is not None and
-                            isinstance(package["versions"], list) and
-                            package["versions"]
-                        ):
-                            versions.update(
-                                map(
-                                    str.strip, package["versions"]
-                                )
-                            )
 
-            if versions:
-                version = sorted(versions, reverse=True)[0] #1,2,3 to next lower version 
-        
+        if not version:
+            version = get_supported_version(config['package'])
+
         if not version:
             version = apkmirror.get_latest_version(app_name)
 
@@ -116,42 +116,22 @@ def download_apkmirror(app_name: str) -> str:
         return download_resource(download_link, filename)
     except Exception as e:
         return None
-        
+
+
 def download_apkpure(app_name: str) -> str:
     global version
 
     try:
-        with open("./patches.json", "r") as patches_file:
-            patches = json.load(patches_file)
-
         conf_file_path = f'./apps/apkpure/{app_name}.json'
 
         with open(conf_file_path, 'r') as json_file:
             config = json.load(json_file)
 
         version = config['version']
-        
-        if not version:
-            versions = set()
-            for patch in patches:
-                compatible_packages = patch.get("compatiblePackages")
-                if compatible_packages and isinstance(compatible_packages, list):
-                    for package in compatible_packages:
-                        if (
-                            package.get("name") == config['package'] and
-                            package.get("versions") is not None and
-                            isinstance(package["versions"], list) and
-                            package["versions"]
-                        ):
-                            versions.update(
-                                map(
-                                    str.strip, package["versions"]
-                                )
-                            )
 
-            if versions:
-                version = sorted(versions, reverse=True)[0] #1,2,3 to next lower version 
-        
+        if not version:
+            version = get_supported_version(config['package'])
+
         if not version:
             version = apkpure.get_latest_version(app_name)
 
@@ -161,42 +141,22 @@ def download_apkpure(app_name: str) -> str:
         return download_resource(download_link, filename)
     except Exception as e:
         return None
-    
+
+
 def download_uptodown(app_name: str) -> str:
     global version
 
     try:
-        with open("./patches.json", "r") as patches_file:
-            patches = json.load(patches_file)
-
         conf_file_path = f'./apps/uptodown/{app_name}.json'
 
         with open(conf_file_path, 'r') as json_file:
             config = json.load(json_file)
 
         version = config['version']
-        
-        if not version:
-            versions = set()
-            for patch in patches:
-                compatible_packages = patch.get("compatiblePackages")
-                if compatible_packages and isinstance(compatible_packages, list):
-                    for package in compatible_packages:
-                        if (
-                            package.get("name") == config['package'] and
-                            package.get("versions") is not None and
-                            isinstance(package["versions"], list) and
-                            package["versions"]
-                        ):
-                            versions.update(
-                                map(
-                                    str.strip, package["versions"]
-                                )
-                            )
 
-            if versions:
-                version = sorted(versions, reverse=True)[0] #1,2,3 to next lower version 
-        
+        if not version:
+            version = get_supported_version(config['package'])
+
         if not version:
             version = uptodown.get_latest_version(app_name)
 
