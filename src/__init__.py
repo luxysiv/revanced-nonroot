ea8923293763fa1f161d98fc7992b24a236bfdaa
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-06-03 16:20:35 +0700

    Custom user-agent

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@ import os
 import logging
 import cloudscraper
 
-base_url = "https://www.apkmirror.com/
+base_url = "https://www.apkmirror.com"
 scraper = cloudscraper.create_scraper()
 scraper.headers.update(
     {'User-Agent': 'Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0'}
