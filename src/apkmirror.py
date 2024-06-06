26484586e6e6482322a394cd83b3af4a67f11e4e
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-06-07 00:01:27 +0700

    Simple logic

--- src/apkmirror.py
+++ src/apkmirror.py
@@ -10,40 +10,37 @@ def get_download_page(version: str, app_name: str) -> str:
     with open(conf_file_path, 'r') as json_file:
         config = json.load(json_file)
 
-    keywords = [config['type'], config['arch'], config['dpi']]
+    criteria = [config['type'], config['arch'], config['dpi']]
     url = (f"{base_url}/apk/{config['org']}/{config['name']}/"
            f"{config['name']}-{version.replace('.', '-')}-release/")
     response = scraper.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.content, "html.parser")
 
-    for href_content in soup.find_all('a', class_='accent_color'):
-        parent = href_content.find_parent('div', class_='table-cell')
-        if parent:
-            infos = [parent.get_text(strip=True)] + [
-                sib.get_text(strip=True) for sib in parent.find_next_siblings('div')
-            ]
-            if all(any(keyword in info for info in infos) for keyword in keywords):
-                return base_url + href_content['href']
-
+    rows = soup.find_all('div', class_='table-row headerFont')
+    for row in rows:
+        row_text = row.get_text()
+        if all(criterion in row_text for criterion in criteria):
+            sub_url = row.find('a', class_='accent_color')
+            if sub_url:
+                return base_url + sub_url['href']
     return None
 
 def extract_download_link(page: str) -> str:
-    
     response = scraper.get(page)
     response.raise_for_status()
     soup = BeautifulSoup(response.content, "html.parser")
 
-    href_content = soup.find('a', class_='downloadButton')
-    if href_content:
-        download_page_url = base_url + href_content['href']
+    sub_url = soup.find('a', class_='downloadButton')
+    if sub_url:
+        download_page_url = base_url + sub_url['href']
         response = scraper.get(download_page_url)
         response.raise_for_status()
         soup = BeautifulSoup(response.content, "html.parser")
 
-        href_content = soup.select_one('a[rel="nofollow"]')
-        if href_content:
-            return base_url + href_content['href']
+        sub_url = soup.select_one('a[rel="nofollow"]')
+        if sub_url:
+            return base_url +  sub_url['href']
 
     return None
 
