8703549de3e979633d563285cc0da20743fb132c
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-06-09 08:19:52 +0700

    Add URL interactive logs

--- src/apkmirror.py
+++ src/apkmirror.py
@@ -1,5 +1,6 @@
 import re
 import json
+import logging 
 
 from bs4 import BeautifulSoup
 from src import base_url, scraper
@@ -15,6 +16,8 @@ def get_download_page(version: str, app_name: str) -> str:
            f"{config['name']}-{version.replace('.', '-')}-release/")
     response = scraper.get(url)
     response.raise_for_status()
+    content_size = len(response.content)
+    logging.info(f"URL:{response.url} [{content_size}/{content_size}] -> \"-\" [1]")
     soup = BeautifulSoup(response.content, "html.parser")
 
     rows = soup.find_all('div', class_='table-row headerFont')
@@ -29,6 +32,8 @@ def get_download_page(version: str, app_name: str) -> str:
 def extract_download_link(page: str) -> str:
     response = scraper.get(page)
     response.raise_for_status()
+    content_size = len(response.content)
+    logging.info(f"URL:{response.url} [{content_size}/{content_size}] -> \"-\" [1]")
     soup = BeautifulSoup(response.content, "html.parser")
 
     sub_url = soup.find('a', class_='downloadButton')
@@ -36,6 +41,8 @@ def extract_download_link(page: str) -> str:
         download_page_url = base_url + sub_url['href']
         response = scraper.get(download_page_url)
         response.raise_for_status()
+        content_size = len(response.content)
+        logging.info(f"URL:{response.url} [{content_size}/{content_size}] -> \"-\" [1]")
         soup = BeautifulSoup(response.content, "html.parser")
 
         sub_url = soup.select_one('a[rel="nofollow"]')
@@ -54,6 +61,8 @@ def get_latest_version(app_name: str) -> str:
 
     response = scraper.get(url)
     response.raise_for_status()
+    content_size = len(response.content)
+    logging.info(f"URL:{response.url} [{content_size}/{content_size}] -> \"-\" [1]")
     soup = BeautifulSoup(response.content, "html.parser")
 
     app_rows = soup.find_all("div", class_="appRow")
