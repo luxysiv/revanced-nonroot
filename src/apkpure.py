4633a52a307fa68c928f275d15b10b1354116830
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-09-03 20:44:08 +0700

    Use latest link

--- src/apkpure.py
+++ src/apkpure.py
@@ -37,7 +37,7 @@ def get_download_link(version: str, app_name: str) ->str:
     content_size = len(response.content)
     logging.info(f"URL:{response.url} [{content_size}/{content_size}] -> \"-\" [1]")
     soup = BeautifulSoup(response.content, "html.parser")
-    download_link = soup.find('a', class_='download-btn')
+    download_link = soup.find('a', id='download_link')
     if download_link:
         return download_link['href']
     
