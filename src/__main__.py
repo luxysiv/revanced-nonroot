05e61b88783cb07d1f85131b340934f46706a418
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-09-25 18:03:16 +0700

    Remove Github release

--- src/__main__.py
+++ src/__main__.py
@@ -90,7 +90,7 @@ def run_build(app_name: str, source: str) -> str:
     os.remove(input_apk_filepath)
 
 
-    release.create_github_release(app_name, source, download_files, output_apk_filepath)
+    # release.create_github_release(app_name, source, download_files, output_apk_filepath)
     
     key = f"{app_name}/{output_apk_filepath}"
     r2.upload(output_apk_filepath, key)
