c908adbe87bd56997ca3289baa20d1e1e1f74cd5
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-12-07 23:02:55 +0700

    Last

--- src/telegram.py
+++ src/telegram.py
@@ -8,8 +8,14 @@ from src import (
 async def upload_file_to_telegram(file_path):
     try:
         bot = Bot(token=bot_token)
+
         with open(file_path, 'rb') as file:
-            await bot.send_document(chat_id=chat_id, document=file)
+            await bot.send_document(
+                chat_id=chat_id,
+                document=file,
+                filename=file_path.split('/')[-1],
+                caption="Here is your file"
+            )
 
         logging.info("Upload to Telegram Channel success!")
     except Exception as e:
