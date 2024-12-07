503c83d827602b25ddcb655c559c2119c3d3ebd9
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-12-07 22:54:53 +0700

    Await telegram

--- src/telegram.py
+++ src/telegram.py
@@ -5,12 +5,12 @@ from src import (
     bot_token
 )
 
-def upload_file_to_telegram(file_path):
+async def upload_file_to_telegram(file_path):
     try:
         bot = Bot(token=bot_token)
         with open(file_path, 'rb') as file:
-            bot.send_document(chat_id=chat_id, document=file)
+            await bot.send_document(chat_id=chat_id, document=file)
 
-        logging.info("Upload to Telegram Chanel success!")
+        logging.info("Upload to Telegram Channel success!")
     except Exception as e:
-        logging.error(f"Error when upload to Telegram: {e}")
+        logging.error(f"Error when uploading to Telegram: {e}")
