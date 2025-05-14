b9a1b7f44163f385bcf3cd3f9e719ed9b46c864f
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2025-05-14 21:03:12 +0700

    Fix

--- src/__main__.py
+++ src/__main__.py
@@ -14,8 +14,8 @@ from src import (
 def run_build(app_name: str, source: str) -> str:
     download_files, name = downloader.download_required(source)
     
-    revanced_cli = utils.find_file(download_files, './revanced-cli', '.jar')
-    revanced_patches = utils.find_file(download_files, './patches', '.rvp')
+    revanced_cli = utils.find_file(download_files, 'revanced-cli', '.jar')
+    revanced_patches = utils.find_file(download_files, 'patches', '.rvp')
 
     download_methods = [
         downloader.download_apkmirror,
