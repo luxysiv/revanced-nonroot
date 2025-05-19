74b74e67881807fcd211eb18a8616aa278e326c9
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2025-05-19 22:47:27 +0700

    Move comment line

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,6 @@ import logging
 import random
 import requests
 
-# Danh sách các User-Agent phổ biến
 user_agents = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
