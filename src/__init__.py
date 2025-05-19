999661ac4164c4db9d34235d16d9e3c6564ba946
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2025-05-19 22:46:41 +0700

    Random User-Agent

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,31 @@
 import os
 import logging
+import random
 import requests
+
+# Danh sách các User-Agent phổ biến
+user_agents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
+    'Mozilla/5.0 (Linux; Android 13; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.6367.93 Mobile Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15',
+    'Mozilla/5.0 (Linux; Android 12; SM-G991B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.6312.105 Mobile Safari/537.36'
+]
+
+random_user_agent = random.choice(user_agents)
 
 base_url = "https://www.apkmirror.com"
 session = requests.Session()
 session.headers.update({
-    'User-Agent': 'Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0'
+    'User-Agent': random_user_agent
 })
 
 # Logging Level
 logging.basicConfig(
-  level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
+    level=logging.INFO,
+    format='%(asctime)s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
 )
 
 github_token = os.getenv('GITHUB_TOKEN')
